68af21359157261e1eab6123af975b2ac84c19b2
okorievictor <74360745+okorievictor@users.noreply.github.com>
2026-06-05 09:44:32 +0100

    fixed deployment issues #2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import './Hero.css'; // CSS File link
+import globeImage from '../../assets/globe.png';
 
 function Hero() {
   const scrollToSection = (sectionId) => {
@@ -10,14 +11,14 @@ function Hero() {
 
   return (
     <section className="hero-container" data-reveal>
-      
+
       {/* Text and Buttons */}
       <div className="hero-content">
         <h1 className="hero-title">Explore Our Solar System Through Data</h1>
         <p className="hero-description">
           Understand the planets not just by name, but by measurable facts. From size and mass to gravity and density, this page breaks down the solar system in a clear, data-driven way.
         </p>
-        
+
         <div className="button-group">
           <button className="btn btn-primary" onClick={() => scrollToSection('planets')}>Explore the Data</button>
           <button className="btn btn-secondary" onClick={() => scrollToSection('contact')}>Contact Us</button>
@@ -26,9 +27,9 @@ function Hero() {
 
       {/* Right Column: Globe Image */}
       <div className="hero-image-wrapper">
-        <img 
-          src="src/assets/globe.png" 
-          alt="Glowing Blue Earth" 
+        <img
+          src={globeImage}
+          alt="Glowing Blue Earth"
           className="earth-image"
         />
       </div>
